1ac1fa78f5cfab991166e840fb6ccc5aa80c1021
Arthas Menethil <50101331+XeNune@users.noreply.github.com>
2021-11-23 14:41:25 +0300

    Add files via upload

---
+++ week1/l1/arif.py
@@ -0,0 +1,29 @@
+plot = 9839
+V = 5
+m = plot * V
+print(m)
+
+#все операции
+a = 2**12
+b = 11 - 6
+c = 6 // 3
+d = 9 % 2
+print(a,b,c,d)
+
+#деление целого + остаток
+G = 50
+H = 11
+CELOE = G // H
+OST = G % H
+print(CELOE,OST)
+
+
+
+# запись чисел со словами
+ansewer = '2 + 3 =' + str(2+3)
+print(ansewer)
+testo = 50
+rice = 100
+
+buylist = 'Макароны ' + str(testo+rice) + 'гр.'
+print(buylist)
